27088c25ef77b6c414f1dfcc49abc69196feb199
anna-sahradyan <a.sahradyan72@gmail.com>
2022-11-11 23:42:43 +0400

    add video on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Search from "./components/Search/Search";
+//import Search from "./components/Search/Search";
+//import { Player } from 'video-react';
+
+
 
 const App = () => {
+
     return (
-        < >
-          
+        <>
+            <div className={"wrapper"}>
+            <div className="video">
+                <video className="video-media" src="/video.mp4/weather.mp4" autoPlay muted
+                       loop></video>
+                <div className={"main"}>
+                    <Search/>
+                </div>
+
+            </div>
+
+            </div>
         </>
     );
 };
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <>
+<h1>WEATHER</h1>
+        </>
+    );
+};
+
+export default Search;
